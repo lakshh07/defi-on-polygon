d8166b803dbb4125ebe95472b31ef15d4cf563e0
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2022-09-02 23:24:23 +0530

    deployed to polygon mumbai 🚀💜

--- Hardhat/hardhat.config.ts
+++ Hardhat/hardhat.config.ts
@@ -1,11 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    mumbai: {
+      url: `${process.env.MUMBAI_ALCHEMY_KEY}`,
+      accounts: [`0x${process.env.PK}`],
     },
   },
 };
